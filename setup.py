6088ce23a0a36a1a1f888e02b83721f0cc5db5f1
danieljfarrell <daniel.james.farrell@gmail.com>
2019-04-18 17:57:23 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,21 @@ setup(
    description='Optical ray tracing for luminescent materials and spectral converter photovoltaic devices.',
    author='Daniel Farrell',
    author_email='dan@excitonlabs.com',
+   url='https://github.com/danieljfarrell/pvtrace',
+   download_url = 'https://github.com/danieljfarrell/pvtrace/archive/v2.0-beta.1.tar.gz'
    python_requires='>=3.7.2',
    packages=find_packages(),
+   keywords=[
+       "optics",
+       "simulation",
+       "ray tracing",
+       "photovoltaics",
+       "solar",
+       "energy",
+       "nonimaging",
+       "luminescence",
+       "spectroscopy"
+   ],
    install_requires=[
        "numpy",
        "scipy",
@@ -15,5 +28,15 @@ setup(
        "anytree",
        "meshcat>=0.0.16",
        "trimesh[easy]"
+   ],
+   classifiers=[
+     'Development Status :: 4 - Beta',
+     'Intended Audience :: Science/Research',
+     'Intended Audience :: Developers',   
+     'Topic :: Scientific/Engineering :: Physics',
+     'Topic :: Scientific/Engineering :: Chemistry',
+     'Topic :: Scientific/Engineering :: Visualization',
+     'License :: OSI Approved :: BSD License',
+     'Programming Language :: Python :: 3.7'
    ]
 )
