9209b72008428bec9cdab79a8c42a3bb26e12268
danieljfarrell <daniel.james.farrell@gmail.com>
2020-02-26 22:22:27 +0000

    Fixes #26 nested cylinders with Meshcat visualiser
    pvtrace uses XYZ basis for definition of a cylinder but meshcat
    uses XZY. This requires applying a transform to render correctly.
    It was easier to do this by transforming to the world node than
    use the parent/child approach. This has the consequence that in
    Meshcat UI the listed scene objects are not nested, but they do
    at least display correctly with repsect to rays.
    
    Also added some options to display wireframe rather than solid
    objects and some option style parameters can be set as kwargs
    to the render object.

---
+++ examples/nested_cylinders.py
@@ -0,0 +1,78 @@
+from pvtrace import (
+    Node,
+    Scene,
+    MeshcatRenderer,
+    Sphere, Box, Cylinder,
+    Material,
+    Surface,
+    FresnelSurfaceDelegate,
+    Light
+)
+import pvtrace.material.utils as phase_functions
+from pvtrace import photon_tracer
+import time
+import functools
+import numpy as np
+import time
+import functools
+import numpy as np
+
+# Add nodes to the scene graph
+world = Node(
+    name="World",
+    geometry=Sphere(
+        radius=10.0,
+        material=Material(refractive_index=1.0),
+    )
+)
+
+cil1 = Node(
+    name="A",
+    geometry=Cylinder(
+        length=2,
+        radius=0.5,
+        material=Material(refractive_index=1.5),
+    ),
+    parent=world
+)
+cil1.translate((0, 0, 2))
+
+cil2 = Node(
+    name="B",
+    geometry=Cylinder(
+        length=0.5,
+        radius=0.4,
+        material=Material(refractive_index=6.0),
+    ),
+    parent=cil1
+)
+
+# Add source of photons
+light = Node(
+    name="Light (555nm)",
+    parent=world,
+    light=Light(
+        direction=functools.partial(
+            phase_functions.cone, np.radians(30)
+        )
+    )
+)
+
+light.translate((0, 0, -1))
+#light.rotate(np.pi/2, (1, 0, 0))
+
+# Use meshcat to render the scene (optional)
+viewer = MeshcatRenderer(open_browser=True, transparency=False, opacity=0.5, wireframe=True)
+scene = Scene(world)
+viewer.render(scene)
+for ray in scene.emit(100):
+    history = photon_tracer.follow(scene, ray)
+    path, events = zip(*history)
+    viewer.add_ray_path(path)  
+
+# Keep the script alive until Ctrl-C (optional)
+while True:
+    try:
+        time.sleep(0.1)
+    except KeyboardInterrupt:
+        break
