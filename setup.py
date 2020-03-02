435c85eb6109c2d4b98a469ec52f939e107d0657
danieljfarrell <daniel.james.farrell@gmail.com>
2020-03-02 01:26:12 +0000

    Reverting BSD License because pypi was not accepting change

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
      'Topic :: Scientific/Engineering :: Physics',
      'Topic :: Scientific/Engineering :: Chemistry',
      'Topic :: Scientific/Engineering :: Visualization',
-     'License :: OSI Approved :: BSD 3-Clause "New" or "Revised" License (BSD-3-Clause)',
+     'License :: OSI Approved :: BSD License',
      'Programming Language :: Python :: 3.7'
    ]
 )
