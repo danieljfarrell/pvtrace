6cb549e9fd050d93d3ea669fab4ebf626e748230
Daniel Farrell <daniel.farrell@teraview.com>
2020-09-09 11:58:47 +0100

    Skip scene renderer test on Windows
    New version of Meshcat seem to require a running ZMQ broker on
    Windows. This tests passes other platforms. Going to skip this
    for now.

--- tests/test_scene_renderer.py
+++ tests/test_scene_renderer.py
@@ -9,9 +9,12 @@ from pvtrace.scene.node import Node
 from pvtrace.scene.scene import Scene
 from pvtrace.scene.renderer import MeshcatRenderer
 
-
+@pytest.mark.skipif(sys.platform == 'win32', reason="Fails on Windows")
 class TestMeshcatRenderer:
     
+    # Meshcat on windows seems to require a running ZMQ broker
+    # this is a change of behaviour introduced some point after 
+    # meshcat v0.0.16
     def test_basic_scene(self):
         a = Node(name="A", parent=None)
         b = Node(name="B", parent=a)
