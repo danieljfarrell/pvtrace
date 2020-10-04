47011006620f8308d75311765484ddf3b18615be
danieljfarrell <daniel.james.farrell@gmail.com>
2020-10-04 19:50:14 +0100

    Add parser for box and sphere
    Cannot fully parse yet the example file yet as the components
    array of material cannot be parsed.

--- cli/parse.py
+++ cli/parse.py
@@ -5,6 +5,8 @@ import jsonschema
 import yaml
 import json
 import os
+import pvtrace
+from pvtrace import Scene, Box, Sphere
 
 
 SCHEMA = os.path.join(
@@ -27,7 +29,7 @@ def load_spec(filename):
         return spec
 
 
-def parse(filename):
+def parse(filename: str) -> Scene:
     schema = load_schema()
     spec = load_spec(filename)
     jsonschema.validate(spec, schema=schema)
@@ -38,12 +40,46 @@ def parse(filename):
         raise ValueError("Version {} not supported".format(schema_version))
 
 
-def parse_v_1_0(spec):
-    print(spec)
+def parse_v_1_0(spec: dict) -> Scene:
+    """Work in progress YAML version 1.0 YAML file parse."""
+
+    def parse_material(spec):
+        refractive_index = spec["refractive-index"]
+        component_names = []
+        if "components" in spec:
+            raise NotImplementedError("Cannot parse components yet")
+
+    def parse_box(spec):
+        print(spec)
+        size = spec["size"]
+        material = parse_material(spec["material"])
+        return Box(size=size, material=material)
+
+    def parse_cylinder(spec):
+        raise NotImplementedError()
+
+    def parse_sphere(spec):
+        radius = spec["radius"]
+        material = parse_material(spec["material"])
+        return Sphere(radius=radius, material=material)
+
+    def parse_mesh(spec):
+        raise NotImplementedError()
+
+    def parse_node(spec):
+        geometry_types = ("box", "cylinder", "sphere", "mesh")
+        geometry_mapper = {"box": parse_box, "sphere": parse_sphere}
+        for geometry_type in geometry_types:
+            if geometry_type in spec:
+                geometry = geometry_mapper[geometry_type](spec[geometry_type])
+
+    node_specs = spec["nodes"]
+    nodes = {k: parse_node(v) for k, v in node_specs.items()}
+    return Scene(nodes["world"])
 
 
 if __name__ == "__main__":
     scene_spec = os.path.join(
         os.path.dirname(os.path.realpath(__file__)), "pvtrace-scene-spec.yml"
     )
-    print("OK")
+    parse(scene_spec)
